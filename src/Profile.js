df5f5ba4b831e4161acf5aeaf3687c4b7656c763
justin_james <jjames11799@gmail.com>
2021-05-16 16:20:34 -0400

    everything

---
+++ src/Profile.js
@@ -0,0 +1,56 @@
+import Button from "@material-ui/core/Button";
+
+const React = require("react");
+const axios = require("axios");
+
+
+const url = "/userprofile"
+
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {}
+    };
+  }
+
+  async componenetDidMount() {
+    try {
+      const response = await axios.get(url);
+      const json = await response.json();
+      this.setState({ user: json });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p>
+        <p>{this.state.user.}</p> */}
+        <Button>Edit Profile: </Button>
+      </div>
+    );
+  }
+}
+
+export default Profile;
+
+
